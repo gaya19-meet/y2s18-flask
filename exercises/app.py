01323d250da4164f2189c0545618fba7d9bb2bd8
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 10:28:47 +0300

    started and finished part 4

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-     return render_template ("index.html")
+     players = ["adele","shakira", "shawn"]
+     return render_template (
+     	"index.html", 
+     	players=players , 
+     	no_players = True)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
